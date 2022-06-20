bf50b0df2b3d1a6d68c7c695ff45d6abec0c2349
alexrcas <xalex1200@gmail.com>
2022-06-20 20:22:24 +0100

    Integrar alta de base de datos en el arranque

--- app.py
+++ app.py
@@ -1,8 +1,19 @@
 from flask import Flask
+import sqlite3
 
 from story.index.indexController import indexController
 from story.rest.generateLink.generateLinkController import generateLinkController
 from story.redirect.redirectController import redirectController
+
+
+connection = sqlite3.connect('database.db')
+with open('schema.sql', 'r') as f:
+    connection.executescript(f.read())
+cur = connection.cursor()
+connection.commit()
+connection.close()
+
+
 
 
 app = Flask(__name__, template_folder='web/templates',
